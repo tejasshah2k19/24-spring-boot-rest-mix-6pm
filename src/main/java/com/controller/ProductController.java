7be615d3ddca2c6e9b3efa8bf5721db9b52d3263
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-13 18:56:32 +0530

    add product into database

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,21 +1,27 @@
 package com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
+import com.repository.ProductRepository;
 
 @RestController
 public class ProductController {
 
+	@Autowired
+	ProductRepository productRepository;
+	
 	// api
 	// read product information and store into databse
 	@PostMapping("/products")
-	public String addProduct(@RequestBody ProductEntity productEntity) {
+	public ProductEntity addProduct(@RequestBody ProductEntity productEntity) {
 		System.out.println(productEntity.getProductName());
 		System.out.println(productEntity.getPrice());
-		return "success";
+		productRepository.save(productEntity);
+		return productEntity;
 	}
 }
 
