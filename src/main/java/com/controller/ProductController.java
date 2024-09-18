6f4b3a524197aec58936c0fc87820a20159be032
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-18 18:38:20 +0530

    list and get by id

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.ProductEntity;
@@ -13,7 +19,7 @@ public class ProductController {
 
 	@Autowired
 	ProductRepository productRepository;
-	
+
 	// api
 	// read product information and store into databse
 	@PostMapping("/products")
@@ -23,7 +29,36 @@ public class ProductController {
 		productRepository.save(productEntity);
 		return productEntity;
 	}
-}
 
+	// read all products information
+	@GetMapping("/products")
+	public List<ProductEntity> getAllProducts() {
+		List<ProductEntity> products = productRepository.findAll();
+		return products;
+	}
+
+	// productId -> fetch
+	@GetMapping("/products/{productId}")
+	public ProductEntity getProductById(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+		if (op.isEmpty()) {
+			return null;
+		} else {
+			ProductEntity productEntity = op.get();
+			return productEntity;
+		}
+	}
+
+	@GetMapping("/productsbyid")
+	public ProductEntity getProductById2(@RequestParam("productId") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+		if (op.isEmpty()) {
+			return null;
+		} else {
+			ProductEntity productEntity = op.get();
+			return productEntity;
+		}
+	}
+}
 
 //http://localhost:9999/products 
