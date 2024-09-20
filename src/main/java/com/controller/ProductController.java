bb40b11bbf5f0e51b17dd64619d919e2a60ee86d
Tejas Shah <tejasshah2k19@gmail.com>
2024-09-20 18:43:36 +0530

    delete by id and update product

--- src/main/java/com/controller/ProductController.java
+++ src/main/java/com/controller/ProductController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +28,7 @@ public class ProductController {
 	public ProductEntity addProduct(@RequestBody ProductEntity productEntity) {
 		System.out.println(productEntity.getProductName());
 		System.out.println(productEntity.getPrice());
-		productRepository.save(productEntity);
+		productRepository.save(productEntity);//insert -> id is not present 
 		return productEntity;
 	}
 
@@ -58,6 +60,29 @@ public class ProductController {
 			ProductEntity productEntity = op.get();
 			return productEntity;
 		}
+	}
+
+	// api -> remove product from products table -> delete product
+	// input : productId
+
+	@DeleteMapping("/products/{productId}")
+	public String deleteProductById(@PathVariable("productId") Integer productId) {
+		Optional<ProductEntity> op = productRepository.findById(productId);
+
+		if (op.isEmpty()) {
+			return "NotFound";
+		} else {
+			productRepository.deleteById(productId);
+			return "success";
+		}
+	}
+
+	// update
+//	input : all product info that we can modify  , productId must be present 
+	@PutMapping("/products")
+	public String updateProduct(@RequestBody ProductEntity productEntity) {
+		productRepository.save(productEntity);//id is present 
+		return "success";
 	}
 }
 
